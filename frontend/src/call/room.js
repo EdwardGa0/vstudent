699d0daa154ec685066fda64a6610711eb3554c9
Jesse Na <jessena.2001@gmail.com>
2021-01-16 15:39:47 -0500

    Test subscriber sound

--- frontend/src/call/room.js
+++ frontend/src/call/room.js
@@ -2,20 +2,21 @@ import React, { Component } from "react";
 import ConnectionStatus from "../opentok/ConnectionStatus";
 import Publisher from "../opentok/Publisher";
 import Subscriber from "../opentok/Subscriber";
-import { OTSession, OTStreams, preloadScript } from "opentok-react";
+import { OTSession, OTStreams, preloadScript, OTSubscriber } from "opentok-react";
 
 export default class room extends Component {
   constructor(props) {
     super(props);
     this.state = {
       loading: true,
-      sessionInfo: null,
+      sessionCredentials: null,
       error: null,
       connected: false,
+      volume: 10
     };
 
     this.subscriberProperties = {
-      audioVolume: 1
+      audioVolume: this.state.volume
     };
 
     this.sessionEvents = {
@@ -32,37 +33,44 @@ export default class room extends Component {
     this.setState({ error: `Failed to connect: ${err.message}` });
   };
 
+  onclick(type){
+    this.setState(prevState => {
+       return {volume: type === 'add' ? prevState.volume + 10: prevState.volume - 10}
+    });
+  }
+
   async componentDidMount() {
-    let headers = new Headers();
-
-
     const url = "https://vstudent-server.herokuapp.com/generate";
-    fetch(url).then(response => response.json()).then(json => this.setState({sessionInfo: json})).catch(function(error) {
-      console.log('Request failed', error)
-  });
-    console.log(this.state.sessionInfo);
+    fetch(url).then(response => response.json()).then(json => this.setState({sessionCredentials: json})).catch(function(error) {
+      console.log('Request failed', error)});
   }
 
   render() {
-    if (!this.state.sessionInfo) {
+    if (!this.state.sessionCredentials) {
       return <div>Error with session retrieval</div>
     }
 
     return (
-      <OTSession
-        apiKey={this.state.sessionInfo.apiKey}
-        sessionId={this.state.sessionInfo.sessionId}
-        token={this.state.sessionInfo.token}
-        eventHandlers={this.sessionEvents}
-        onError={this.onError}
-      >
-        {this.state.error ? <div id="error">{this.state.error}</div> : null}
-        <ConnectionStatus connected={this.state.connected} />
-        <Publisher />
-        <OTStreams>
-          <Subscriber properties={this.subscriberProperties}/>
-        </OTStreams>
-      </OTSession>
+      <div>
+        <div>Volume: {this.state.volume}
+          <input type='button' onClick={this.onclick.bind(this, 'add')} value='Inc'/>
+          <input type='button' onClick={this.onclick.bind(this, 'sub')} value='Dec'/>
+        </div>
+        <OTSession
+          apiKey={this.state.sessionCredentials.apiKey}
+          sessionId={this.state.sessionCredentials.sessionId}
+          token={this.state.sessionCredentials.token}
+          eventHandlers={this.sessionEvents}
+          onError={this.onError}
+        >
+          {this.state.error ? <div id="error">{this.state.error}</div> : null}
+          <ConnectionStatus connected={this.state.connected} />
+          <Publisher />
+          <OTStreams>
+            <OTSubscriber properties={this.subscriberProperties}/>
+          </OTStreams>
+        </OTSession>
+      </div>
     );
   }
 }
