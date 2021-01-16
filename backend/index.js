2cf5206ce34b0443e44b426187f8ec6e4bb5aa42
Jesse Na <jessena.2001@gmail.com>
2021-01-16 13:02:55 -0500

    Add cors allow cross origin

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 require('dotenv').config();
 const app = require('express')();
 const OpenTok = require("opentok");
+const cors = require('cors');
 
 const opentok = new OpenTok(process.env.API_KEY, process.env.API_SECRET);
+
+// var whitelist = ['http://localhost:3000/']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+
+app.use(cors());
 
 // Create a session and store it in the express app
 opentok.createSession(function(err, session) {
@@ -13,6 +27,10 @@ opentok.createSession(function(err, session) {
 });
 
 app.get('/', function(req, res) {
+    res.send("Ping");
+});
+
+app.get('/generate', function(req, res) {
     var sessionId = app.get('sessionId'),
         // generate a fresh token for this client
         token = opentok.generateToken(sessionId);
@@ -25,7 +43,7 @@ app.get('/', function(req, res) {
   });
 
   function init() {
-    app.listen(3000, function() {
-      console.log('You\'re app is now ready at http://localhost:3000/');
+    app.listen(5000, function() {
+      console.log('You\'re app is now ready at http://localhost:5000/');
     });
   }
